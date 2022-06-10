bba1d6cad6c76e626889e6281b4f4cc64ff56529
Willem Zen <willem_zen@example.com>
2022-06-10 18:25:44 +0200

    Compare type Value {.float, .string, .empty}

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const testing = std.testing;
 
 const common = @import("common.zig");
+const Compare = common.Compare;
 const croc = common.allocator;
 
 const Type = enum {
@@ -30,6 +31,68 @@ const Value = union(Type) {
             try v.string.appendSlice(s);
             return v;
         }
+    }
+
+    fn compare(self: Self, other: Value) Compare {
+        _ = switch (self) {
+            .string => |self_s| {
+                _ = switch (other) {
+                    .string => |other_s| return common.strcmp(self_s.items, other_s.items),
+                    .float => |other_f| {
+                        var other_line = std.ArrayList(u8).init(croc);
+                        defer other_line.deinit();
+                        other_line.writer().print("{d}", .{other_f}) catch {
+                            @panic("out of memory");
+                        };
+                        return common.strcmp(self_s.items, other_line.items);
+                    },
+                    .empty => return Compare.greater,
+                };
+            },
+            .float => |self_f| {
+                _ = switch (other) {
+                    .float => |other_f| return if (self_f > other_f) Compare.greater else if (self_f < other_f) Compare.lesser else Compare.equal,
+                    .string => |other_s| {
+                        var self_line = std.ArrayList(u8).init(croc);
+                        defer self_line.deinit();
+                        self_line.writer().print("{d}", .{self_f}) catch {
+                            @panic("out of memory");
+                        };
+                        return common.strcmp(self_line.items, other_s.items);
+                    },
+                    .empty => return Compare.greater,
+                };
+            },
+            .empty => {
+                _ = switch (other) {
+                    .empty => return Compare.equal,
+                    else => return Compare.lesser,
+                };
+            },
+        };
+    }
+
+    test "Value.compare" {
+        const fa = Value{ .float = 1 };
+        const fb = Value{ .float = 2 };
+        var sa = Value{ .string = std.ArrayList(u8).init(croc) };
+        defer sa.string.deinit();
+        try sa.string.writer().print("{d}", .{1});
+        var sb = Value{ .string = std.ArrayList(u8).init(croc) };
+        defer sb.string.deinit();
+        try sb.string.writer().print("{d}", .{2});
+
+        // float <> float
+        try testing.expectEqual(fa.compare(fb), Compare.lesser);
+        try testing.expectEqual(fb.compare(fa), Compare.greater);
+
+        // str <> float
+        try testing.expectEqual(fa.compare(sa), Compare.equal);
+        try testing.expectEqual(fb.compare(sa), Compare.greater);
+
+        // str <> str
+        try testing.expectEqual(sa.compare(sa), Compare.equal);
+        try testing.expectEqual(sb.compare(sa), Compare.greater);
     }
 
     fn deinit(self: Self) void {
